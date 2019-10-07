cefa0270918a8ec1bc4d020891b2cc7e124b36fe
levi davis <levidavis@pursuit.org>
2019-10-07 15:48:28 -0400

    Adds a label programatically

--- lab-programatic-color-guess/ViewController.swift
+++ lab-programatic-color-guess/ViewController.swift
@@ -12,11 +12,22 @@ class ViewController: UIViewController {
     
     let colorModel = ColorGuessingModel()
     
+    lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = "Guess the Dominant Color!"
+        label.textColor = .black
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.textAlignment = .center
+        return label
+    }()
+    
     lazy var redButton: UIButton = {
         let redButton = UIButton()
         redButton.backgroundColor = .red
         redButton.setTitle("Red", for: .normal)
         redButton.setTitleColor(.black, for: .normal)
+        redButton.tag = 1
         redButton.addTarget(self, action: #selector(redButtonPressed(sender:)), for: .touchUpInside)
 
         return redButton
@@ -27,6 +38,7 @@ class ViewController: UIViewController {
         blueButton.backgroundColor = .blue
         blueButton.setTitle("Blue", for: .normal)
         blueButton.setTitleColor(.black, for: .normal)
+        blueButton.tag = 2
         blueButton.addTarget(self, action: #selector(blueButtonPressed(sender:)), for: .touchUpInside)
         return blueButton
     }()
@@ -36,6 +48,7 @@ class ViewController: UIViewController {
         greenButton.backgroundColor = .green
         greenButton.setTitle("Green", for: .normal)
         greenButton.setTitleColor(.black, for: .normal)
+        greenButton.tag = 3
         greenButton.addTarget(self, action: #selector(greenButtonPressed(sender:)), for: .touchUpInside)
         return greenButton
     }()
@@ -49,37 +62,56 @@ class ViewController: UIViewController {
     
     
     private func addSubViews() {
-        redButton.translatesAutoresizingMaskIntoConstraints = false
-        blueButton.translatesAutoresizingMaskIntoConstraints = false
-        greenButton.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(label)
         self.view.addSubview(redButton)
         self.view.addSubview(blueButton)
         self.view.addSubview(greenButton)
     }
     
     private func addConstraintsToSubViews() {
-        redButton.centerXAnchor.constraint(equalToSystemSpacingAfter: self.view.centerXAnchor, multiplier: 1).isActive = true
-        redButton.centerYAnchor.constraint(equalToSystemSpacingBelow: self.view.centerYAnchor, multiplier: 1).isActive = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        redButton.translatesAutoresizingMaskIntoConstraints = false
+        blueButton.translatesAutoresizingMaskIntoConstraints = false
+        greenButton.translatesAutoresizingMaskIntoConstraints = false
+        redButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        redButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
         redButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
-        blueButton.centerXAnchor.constraint(equalToSystemSpacingAfter: self.view.centerXAnchor, multiplier: 1).isActive = true
+        blueButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
         blueButton.topAnchor.constraint(equalTo: self.redButton.bottomAnchor, constant: 10).isActive = true
         blueButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
-        greenButton.centerXAnchor.constraint(equalToSystemSpacingAfter: self.view.centerXAnchor, multiplier: 1).isActive = true
+        greenButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
         greenButton.topAnchor.constraint(equalTo: self.blueButton.bottomAnchor, constant: 10).isActive = true
         greenButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        label.bottomAnchor.constraint(equalTo: self.redButton.topAnchor, constant: -50).isActive = true
         
     }
     
     @objc private func redButtonPressed(sender: UIButton!) {
-        self.view.backgroundColor = .red
+        if colorModel.isDominant(guess: .red) {
+            
+            print("yes")
+        } else {
+            print("no")
+        }
     }
     
     @objc private func blueButtonPressed(sender: UIButton!) {
-        self.view.backgroundColor = .blue
+        if colorModel.isDominant(guess: .blue) {
+            print("yes")
+        } else {
+            print("no")
+        }
+        
     }
     
     @objc private func greenButtonPressed(sender: UIButton!) {
-        self.view.backgroundColor = .green
+        if colorModel.isDominant(guess: .green) {
+            print("yes")
+        } else {
+            print("no")
+        }
+        
     }
     
     private func setDefaultBackgroundColor() {
